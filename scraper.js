19402f0bbf613a7a839bcb152b1bba013bb2544a
Caley Woods <caley@vcreativeinc.com>
2021-12-21 08:58:13 -0600

    Initial commit

---
+++ scraper.js
@@ -0,0 +1,62 @@
+const fs = require('fs/promises');
+const puppeteer = require('puppeteer');
+const YAML = require('json2yaml');
+
+const config = {
+    output_dir: './state_files',
+    targetURL: 'https://childcare.gov/state-resources?state=',
+    puppeteer: {
+        headless: false
+    },
+    stateSections: [
+        '201',
+        '202',
+        '203',
+        '204'
+    ]
+};
+
+// 201 = "Understanding and Finding Child Care"
+// 202 = "Financial Assistance For Families"
+// 203 = "Health & Social Services"
+// 204 = "Child Development & Early Living"
+
+const stateData = new Map([
+    [6, "Alabama"],
+    [7, "Alaska"],
+    [8, "Arizona"],
+    [9, "Arkansas"],
+    [10, "California"],
+    [11, "Colorado"],
+    [12, "Connecticut"],
+    [13, "Delaware"],
+    [14, "Florida"],
+    [15, "Georgia"],
+]);
+
+(async () => {
+    const browser = await puppeteer.launch(config.puppeteer);
+    const page = await browser.newPage();
+
+    for (const [stateID, stateName] of stateData) {
+        for (const sectionID of config.stateSections) {
+            await page.goto(`${config.targetURL}${stateID}&type=${sectionID}`, {waitUntil: 'networkidle2'});
+            const linkSections = await page.evaluate((sel) => {
+                let els = Array.from(document.querySelectorAll(sel));
+                let sectionData = els.map(el => {
+                    // First child of each link section is the section title
+                    const data = {title: el.children[0].innerText, links:[]};
+                    const links = Array.from(el.querySelectorAll('.list-links > ul > li > a:not(.exit_link)'));
+                    links.map(li => {
+                        return data.links.push({text: li.innerText, href: li.href});
+                    });
+                    return data;
+                });
+                return sectionData;
+            }, '.col-sm-6:not(.col-lg-4)');
+
+            await fs.writeFile(`${config.output_dir}/${stateName}-${sectionID}.yml`, YAML.stringify(linkSections));
+        }
+    }
+    browser.close();
+})();
